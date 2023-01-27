152e5db6b26774fd6caa47807feb57295d42d2ea
Kratik Soni <kratiksoni1104@gmail.com>
2023-01-27 15:56:03 +0530

    Enviornment variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -19,8 +20,7 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-const secret = "ThisIsOurLittleSecret";
-userSchema.plugin(encrypt , {secret : secret , encryptedFields : ["password"]});
+userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ["password"]});
 
 const User = mongoose.model("User" , userSchema);
 
